59eae618d57b58f5d10baaf67a16a6dce3aae329
lopesc06 <lopesc06@hotmail.com>
2018-07-30 20:40:25 -0500

    First commit Asp.net core

---
+++ CityInfo.API/Cityinfo.API/CitiesDataStore.cs
@@ -0,0 +1,40 @@
+﻿using Cityinfo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cityinfo.API
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id=1,
+                    Name = "USA",
+                    Description = "USA Description"
+                },
+                new CityDto()
+                {
+                    Id=2,
+                    Name = "Mexico",
+                    Description = "Mexico Description"
+                },
+                new CityDto()
+                {
+                    Id=3,
+                    Name = "Chile",
+                    Description = "Chile Description"
+                },
+
+            };
+        }
+    }
+}
---
+++ CityInfo.API/Cityinfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cityinfo.API.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+        [HttpGet()]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(
+                CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id)
+            );
+        }
+
+    }
+}
